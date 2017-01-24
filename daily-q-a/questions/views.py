269b13924aed20375cf3cf26a993c3e45dea5ca5
Ally Weir <ally.pcgf@gmail.com>
2017-01-24 19:56:22 +0000

    Add page to show if already submitted today

--- daily-q-a/questions/views.py
+++ daily-q-a/questions/views.py
@@ -1,23 +1,43 @@
 from django.shortcuts import render
 from django.core.urlresolvers import reverse_lazy, reverse
 from django.http import HttpResponse, HttpResponseRedirect
-from django.views.generic import TemplateView, FormView
+from django.views.generic import TemplateView, FormView, View
 
 from braces.views import LoginRequiredMixin
 
 from .forms import DailyPromptForm
-from .models import Question
+from .models import Question, Response
 
 import time
 
-class TodayView(LoginRequiredMixin, FormView):
+class TodayView(LoginRequiredMixin, View):
+
+    def get(self, request, *args, **kwargs):
+        current_question = Question.objects.get(date = time.strftime("%m/%d"))
+        has_answered = Response.objects.filter(question=current_question).filter(user=self.request.user).count() > 0
+
+        if (has_answered):
+            view = TodayComplete.as_view()
+        else:
+            view = TodayForm.as_view()
+
+        return view(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        view = TodayForm.as_view()
+        return view(request, *args, **kwargs)
+
+class TodayComplete(LoginRequiredMixin, TemplateView):
+    template_name = "questions/today_already_submitted.html"
+
+class TodayForm(LoginRequiredMixin, FormView):
     template_name = "questions/today.html"
     form_class = DailyPromptForm
     success_url = reverse_lazy("questions:today_success")
 
 
     def get_context_data(self, **kwargs):
-        context = super(TodayView, self).get_context_data(**kwargs)
+        context = super(TodayForm, self).get_context_data(**kwargs)
 
         context["today_date"] = time.strftime("%m/%d")
         context['today_question'] = Question.objects.get(date=context["today_date"])
